cfd5c40c783c1559fdbc297cd447918c39d359bf
tshimsthedoor <tshims79@gmail.com>
2023-12-14 21:47:27 +0200

    done with migration part

--- fooddeliveryapp/database/migrations/2023_12_14_164428_products.php
+++ fooddeliveryapp/database/migrations/2023_12_14_164428_products.php
@@ -17,7 +17,6 @@ return new class extends Migration
                     $table->string('description');
                     $table->string('price');
                     $table->string('image');
-                    $table->rememberToken();
                     $table->timestamps();
                 });
     }
